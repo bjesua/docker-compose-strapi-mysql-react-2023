9807d4b83d4834e27e00afa60271b78786e94ca5
js <bjesua@gmail.com>
2023-01-01 21:46:39 -0600

    fix docker-compose

--- client/src/components/sample-component/sample-component.jsx
+++ client/src/components/sample-component/sample-component.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import axios from 'axios';
 
 function callServer() {
-  axios.get(`http://localhost:${process.env.REACT_APP_SERVER_PORT}/test`, {
-    params: {
-      table: 'sample',
-    },
+  axios.get(`http://localhost:1337/usuarios`, {
+    // axios.get(`http://localhost:${process.env.REACT_APP_SERVER_PORT}/test`, {
+    // params: {
+    //   table: 'sample',
+    // },
   }).then((response) => {
     console.log(response.data);
   });
