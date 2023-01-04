d974b690bf9be9fac44807b03ab7c57e238742ee
js <bjesua@gmail.com>
2023-01-03 21:43:17 -0600

    database

--- app/config/database.js
+++ app/config/database.js
@@ -5,10 +5,10 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', 'db'),
+        host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'root'),
         password: env('DATABASE_PASSWORD', 'strapi'),
         ssl: env.bool('DATABASE_SSL', false),
       },
