c981fd7a143d862f87c766350ff318f173dd9dc1
js <bjesua@gmail.com>
2023-01-04 23:14:33 -0600

    change pw

--- app/config/database.js
+++ app/config/database.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'strapi'),
         username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'Strap!2023'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
